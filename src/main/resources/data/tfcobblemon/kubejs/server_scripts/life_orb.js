e6d6795d9835967ed60abfae20180de548a6026f
Axten <axten.dev@gmail.com>
2026-09-09 12:30:42 +0200

    sistemo gli script kubejs per kubejs 7
    Utils.server non c'e' piu': si usa event.server, e runCommandSilent vuole il
    comando senza slash iniziale. Il danno dell'item non si passa piu' via NBT
    ma con damageValue, visto che 1.21 usa i data component.
    
    Tolgo anche tfcobblemon.json, rimasto vuoto e mai letto da niente.

--- src/main/resources/data/tfcobblemon/kubejs/server_scripts/life_orb.js
+++ src/main/resources/data/tfcobblemon/kubejs/server_scripts/life_orb.js
@@ -2,8 +2,10 @@
 
 ItemEvents.rightClicked('tfcobblemon:blank_orb', event=> {
         if (event.player.mainHandItem.id == "tfcobblemon:blank_orb") {
-            event.player.mainHandItem = Item.of('tfcobblemon:life_orb_charging', '{Damage:100}');
-            Utils.server.runCommandSilent(`/playsound minecraft:entity.allay.ambient_with_item block ${event.player.username} ${event.player.x} ${event.player.y} ${event.player.z} 3`)
+            const orb = Item.of('tfcobblemon:life_orb_charging');
+            orb.damageValue = 100;
+            event.player.mainHandItem = orb;
+            event.server.runCommandSilent(`playsound minecraft:entity.allay.ambient_with_item block ${event.player.username} ${event.player.x} ${event.player.y} ${event.player.z} 3`)
     }
 })
 
@@ -11,7 +13,7 @@ ItemEvents.rightClicked('tfcobblemon:life_orb_charging', event=> {
   if (event.player.mainHandItem.id == "tfcobblemon:life_orb_charging") {
     if (event.player.mainHandItem.damageValue <= 0) {
       event.player.mainHandItem = Item.of('cobblemon:life_orb');
-      Utils.server.runCommandSilent(`/playsound minecraft:entity.allay.ambient_without_item block ${event.player.username} ${event.player.x} ${event.player.y} ${event.player.z} 3`)
+      event.server.runCommandSilent(`playsound minecraft:entity.allay.ambient_without_item block ${event.player.username} ${event.player.x} ${event.player.y} ${event.player.z} 3`)
     }
 }
 })
@@ -24,7 +26,7 @@ EntityEvents.death('cobblemon:pokemon', event => {
             let orb = nearestPlayer.inventory.getItem(index);
             if (orb.damageValue >= 1) {
                 orb.damageValue--;
-                Utils.server.runCommandSilent(`/playsound minecraft:block.amethyst_cluster.hit block ${nearestPlayer.username} ${nearestPlayer.x} ${nearestPlayer.y} ${nearestPlayer.z} 3`);
+                event.server.runCommandSilent(`playsound minecraft:block.amethyst_cluster.hit block ${nearestPlayer.username} ${nearestPlayer.x} ${nearestPlayer.y} ${nearestPlayer.z} 3`);
             }
         }
     }
--- src/main/resources/data/tfcobblemon/kubejs/server_scripts/salt_water_breathing.js
+++ src/main/resources/data/tfcobblemon/kubejs/server_scripts/salt_water_breathing.js
@@ -1,7 +1,6 @@
 EntityEvents.spawned("cobblemon:pokemon", event => {
     const {entity, player, server} = event
-    let nbt = entity.serializeNBT();
-    let species = nbt.Pokemon.Species.toString().replace("cobblemon:", "");
+    let species = entity.fullNBT.Pokemon.Species.toString().replace("cobblemon:", "");
     // Check if the species is in the speciesList array
     if ((global.saltWaterBreather).includes(species)) {
         if (!entity.hasEffect("minecraft:water_breathing")) {
